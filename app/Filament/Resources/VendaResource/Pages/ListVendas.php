898e8a559d892c4d09176cf68c75736c2760fc64
Marcus Moreira de Souza <marcus.moreira@gmail.com>
2024-04-15 13:53:46 -0300

    Traduções

--- app/Filament/Resources/VendaResource/Pages/ListVendas.php
+++ app/Filament/Resources/VendaResource/Pages/ListVendas.php
@@ -5,15 +5,18 @@ namespace App\Filament\Resources\VendaResource\Pages;
 use App\Filament\Resources\VendaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Str;
 
 class ListVendas extends ListRecords
 {
     protected static string $resource = VendaResource::class;
 
+    //protected static ?string $title = "Teste";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label('Registrar nova venda'),
         ];
     }
 }
